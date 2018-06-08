86ca44a351f14881bc25a63744a6bd660aa629a6
innovationchef <ankit.lohani.iitkgp@gmail.com>
2018-06-08 06:50:12 +0530

    minor fixes

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import zipfile
 from lxml import etree
 from xml.dom import minidom
 from parsy.parser import Parser 
+from collections import OrderedDict
 
 
-i = 9
+i = 1
 filename = str(i)+'.docx'
 
 # Validate input file
@@ -40,15 +41,52 @@ root = tree.getroot()
 
 # Save a pretty printed xml in the debugger directory
 xmlstr = minidom.parseString(etree.tostring(tree)).toprettyxml(indent="   ")
-with open("debugger/sample.xml", "w") as f:
+with open("cache/debugger/sample.xml", "w") as f:
     f.write(xmlstr)
 
 # Parse file
 parser = Parser(root)
 resume = parser.master
-print(resume)
 
 # Get default resume fields
-with open('data.json') as f:
-    data = json.load(f)
-    
+config = 'config/default.json'
+with open(config) as f:
+    resume_config = json.load(f)
+
+print(resume)
+
+# parsed = OrderedDict()
+
+# def similarity_single_lines(section, key, keywords, line):
+# 	line = line[0].lower()
+# 	for word in keywords:
+# 		word = word.lower()
+# 		if (line.find(word) == -1):
+# 			parsed[line] = "TBD"
+# 		else:
+# 			parsed[line] = section
+
+# def similarity_multiple_lines(section, key, keywords, line):
+# 	line = ' '.join(line).lower()
+# 	for word in keywords:
+# 		word = word.lower()
+# 		if (line.find(word) == -1):
+# 			parsed[line] = "TBD"
+# 		else:
+# 			parsed[line] = section
+
+# for i, line in enumerate(resume):
+# 	if len(line) == 1:
+# 		for section, attribute in resume_config.items():
+# 			for key, keywords in attribute.items():
+# 				similarity_single_lines(section, key, keywords, line)
+# 	else:
+# 		if isinstance(line[0], list):
+# 			parsed[line[0][0]] = "table"
+# 		elif isinstance(line[0], str):
+# 			for section, attribute in resume_config.items():
+# 				for key, keywords in attribute.items():
+# 					similarity_multiple_lines(section, key, keywords, line)
+
+# for key, val in parsed.items():
+# 	print(key, "******>", val)
