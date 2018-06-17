37a2b7507df841e97160607b59b72869166607e2
innovationchef <ankit.lohani.iitkgp@gmail.com>
2018-06-17 12:44:59 +0530

    Integrated backend with frontend

--- app.py
+++ app.py
@@ -1,13 +1,87 @@
-from flask import Flask, render_template, url_for
-
+import os
+from main import ConnectFlask
+from werkzeug import secure_filename
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = '/home/innovationchef/Desktop/resume-parser/data/flask/'
+ALLOWED_EXTENSIONS = set(['docx'])
 
-@app.route("/")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['GET', 'POST'])
 def main():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index', file=filename))
+    return """
+            <!doctype html>
+            <title>Upload new File</title>
+            <h1>Upload new File</h1>
+            <form action="" method=post enctype=multipart/form-data>
+              <p><input type=file name=file>
+                 <a href={{ url_for('index') }}> <input type=submit value=Upload> </a>
+            </form>
+            <p>%s</p>
+            """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
+
+@app.route("/index/<file>")
+def index(file):
+    app.logger.info(file)
+    Resume = ConnectFlask(file)
+    print(Resume.parse())
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask, render_template, url_for
+
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def main():
+#     return render_template('index.html')
+
+# if __name__ == "__main__":
+#     app.debug = True
+#     app.run()
--- main.py
+++ main.py
@@ -2,91 +2,80 @@ import os
 import json
 import shutil
 import zipfile
+import  numpy as np
 from lxml import etree
 from xml.dom import minidom
 from parsy.parser import Parser 
 from collections import OrderedDict
 
 
-i = 1
-filename = str(i)+'.docx'
+class ConnectFlask():
+    def __init__(self, filename):
+        self.filename = filename
 
-# Validate input file
-ext = os.path.splitext(filename)[-1].lower()
-if ext != ".docx":
-    exit()
+    def parse(self):
+        filename = self.filename
 
-# Extract file initial name
-name = os.path.splitext(os.path.basename(filename))[0]
+        # Validate input file
+        ext = os.path.splitext(filename)[-1].lower()
+        if ext != ".docx":
+            exit()
 
-# input Folder
-input_base_dir = 'data/input_doc/'
+        # Extract file initial name
+        name = os.path.splitext(os.path.basename(filename))[0]
 
-# cache folder 
-cache_path = 'cache/cv.zip'
+        # input Folder
+        input_base_dir = 'data/flask/'
 
-# Make temporary directory in cache folder
-shutil.copy(input_base_dir+filename, cache_path)
-with zipfile.ZipFile(cache_path,"r") as zip_ref:
-    zip_ref.extractall("cache/cv")
-if os.path.isfile(cache_path):
-    os.remove(cache_path)
+        # cache folder 
+        cache_path = 'cache/cv.zip'
 
-# Locate the xml file to be parsed
-doc_xml_path = 'cache/cv/word/document.xml'
+        # Make temporary directory in cache folder
+        shutil.copy(input_base_dir+filename, cache_path)
+        with zipfile.ZipFile(cache_path,"r") as zip_ref:
+            zip_ref.extractall("cache/cv")
+        if os.path.isfile(cache_path):
+            os.remove(cache_path)
 
-# Parse the XML
-tree = etree.parse(doc_xml_path)
-root = tree.getroot()
+        # Locate the xml file to be parsed
+        doc_xml_path = 'cache/cv/word/document.xml'
 
-# Save a pretty printed xml in the debugger directory
-xmlstr = minidom.parseString(etree.tostring(tree)).toprettyxml(indent="   ")
-with open("cache/debugger/sample.xml", "w") as f:
-    f.write(xmlstr)
+        # Parse the XML
+        tree = etree.parse(doc_xml_path)
+        root = tree.getroot()
 
-# Parse file
-parser = Parser(root)
-resume = parser.master
+        # Save a pretty printed xml in the debugger directory
+        xmlstr = minidom.parseString(etree.tostring(tree)).toprettyxml(indent="   ")
+        with open("cache/debugger/sample.xml", "w") as f:
+            f.write(xmlstr)
 
-# Get default resume fields
-config = 'config/default.json'
-with open(config) as f:
-    resume_config = json.load(f)
+        # Parse file
+        parser = Parser(root)
+        resume = parser.master
 
-print(resume)
+        return resume
 
-# parsed = OrderedDict()
+        ###############################################################################
 
-# def similarity_single_lines(section, key, keywords, line):
-# 	line = line[0].lower()
-# 	for word in keywords:
-# 		word = word.lower()
-# 		if (line.find(word) == -1):
-# 			parsed[line] = "TBD"
-# 		else:
-# 			parsed[line] = section
+        # config = 'config/default.json'
+        # with open(config) as f:
+        #     resume_config = json.load(f)
+        
+        # def match(keywords, item):
+        #   for keyword in keywords:
+        #       if item[0].lower().find(keyword.lower()) != -1:
+        #           print(keyword, item)
 
-# def similarity_multiple_lines(section, key, keywords, line):
-# 	line = ' '.join(line).lower()
-# 	for word in keywords:
-# 		word = word.lower()
-# 		if (line.find(word) == -1):
-# 			parsed[line] = "TBD"
-# 		else:
-# 			parsed[line] = section
-
-# for i, line in enumerate(resume):
-# 	if len(line) == 1:
-# 		for section, attribute in resume_config.items():
-# 			for key, keywords in attribute.items():
-# 				similarity_single_lines(section, key, keywords, line)
-# 	else:
-# 		if isinstance(line[0], list):
-# 			parsed[line[0][0]] = "table"
-# 		elif isinstance(line[0], str):
-# 			for section, attribute in resume_config.items():
-# 				for key, keywords in attribute.items():
-# 					similarity_multiple_lines(section, key, keywords, line)
-
-# for key, val in parsed.items():
-# 	print(key, "******>", val)
+        # for line in resume:
+        #   if len(line) == 1:
+        #       for sec, subsec in resume_config.items():
+        #           for key, keywords in subsec.items():
+        #               match(keywords, line)
+        #               # print("******************************LINE")
+        #   else:
+        #       try:
+        #           r, c = np.array(line).shape
+        #           print("table")
+        #       except ValueError:
+        #           print("List of lists")
+        #           
